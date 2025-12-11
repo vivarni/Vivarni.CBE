358b8f6fc60429ccc1e871488c38f31f9bae8ed3
Michaël Vittorelli <vittorelli@gmail.com>
2025-12-11 12:47:37 +0100

    Add empty test projects

---
+++ tests/Vivarni.CBE.Test/ServiceProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vivarni.CBE.Test;
+
+internal class ServiceProviderFactory
+{
+    public static IServiceProvider Create(Action<IServiceCollection>? configure = null)
+    {
+        var services = new ServiceCollection();
+        configure?.Invoke(services);
+
+        return services.BuildServiceProvider();
+    }
+}
