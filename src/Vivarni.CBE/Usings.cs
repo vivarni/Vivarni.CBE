3dffcab762292f1ef97c74c7844a905172db7475
Michaël Vittorelli <vittorelli@gmail.com>
2025-12-11 13:04:21 +0100

    CbeDataSourceProxyTests

--- src/Vivarni.CBE/Usings.cs
+++ src/Vivarni.CBE/Usings.cs
@@ -1,3 +1,6 @@
 ﻿global using Vivarni.CBE.DataAnnotations;
 global using Vivarni.CBE.DataSources.Mappings;
 global using CsvIndexAttribute = CsvHelper.Configuration.Attributes.IndexAttribute;
+
+// So our unit tests can also test internal classes
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Vivarni.CBE.Test")]
---
+++ tests/Vivarni.CBE.Test/DataSources/CbeDataSourceProxyTests.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Vivarni.CBE.DataSources;
+using Xunit;
+
+namespace Vivarni.CBE.Test.DataSources;
+
+public class CbeDataSourceProxyTests
+{
+    private readonly Mock<ICbeDataSource> _mockSource;
+    private readonly Mock<ICbeDataSource> _mockCache;
+    private readonly CancellationToken _cancellationToken;
+
+    public CbeDataSourceProxyTests()
+    {
+        _mockSource = new Mock<ICbeDataSource>();
+        _mockCache = new Mock<ICbeDataSource>();
+        _cancellationToken = CancellationToken.None;
+    }
+
+    #region GetOpenDataFilesAsync Tests
+
+    [Fact]
+    public async Task GetOpenDataFilesAsync_WithBothSources_ShouldCombineResults()
+    {
+        // Arrange
+        var sourceFiles = new List<CbeOpenDataFile>
+        {
+            new("KboOpenData_0001_2025_01_01_Full.zip"),
+            new("KboOpenData_0002_2025_01_02_Update.zip")
+        };
+        var cacheFiles = new List<CbeOpenDataFile>
+        {
+            new("KboOpenData_0002_2025_01_02_Update.zip"), // Duplicate
+            new("KboOpenData_0003_2025_01_03_Full.zip")
+        };
+
+        _mockSource.Setup(s => s.GetOpenDataFilesAsync(_cancellationToken))
+            .ReturnsAsync(sourceFiles);
+        _mockCache.Setup(c => c.GetOpenDataFilesAsync(_cancellationToken))
+            .ReturnsAsync(cacheFiles);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, _mockCache.Object);
+
+        // Act
+        var result = await proxy.GetOpenDataFilesAsync(_cancellationToken);
+
+        // Assert
+        Assert.Equal(3, result.Count); // Should deduplicate
+        Assert.Contains(result, f => f.Filename == "KboOpenData_0001_2025_01_01_Full.zip");
+        Assert.Contains(result, f => f.Filename == "KboOpenData_0002_2025_01_02_Update.zip");
+        Assert.Contains(result, f => f.Filename == "KboOpenData_0003_2025_01_03_Full.zip");
+    }
+
+    [Fact]
+    public async Task GetOpenDataFilesAsync_WithSourceOnly_ShouldReturnSourceResults()
+    {
+        // Arrange
+        var sourceFiles = new List<CbeOpenDataFile>
+        {
+            new("KboOpenData_0001_2025_01_01_Full.zip")
+        };
+
+        _mockSource.Setup(s => s.GetOpenDataFilesAsync(_cancellationToken))
+            .ReturnsAsync(sourceFiles);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, null);
+
+        // Act
+        var result = await proxy.GetOpenDataFilesAsync(_cancellationToken);
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal("KboOpenData_0001_2025_01_01_Full.zip", result.First().Filename);
+    }
+
+    [Fact]
+    public async Task GetOpenDataFilesAsync_WithCacheOnly_ShouldReturnCacheResults()
+    {
+        // Arrange
+        var cacheFiles = new List<CbeOpenDataFile>
+        {
+            new("KboOpenData_0001_2025_01_01_Full.zip")
+        };
+
+        _mockCache.Setup(c => c.GetOpenDataFilesAsync(_cancellationToken))
+            .ReturnsAsync(cacheFiles);
+
+        var proxy = new CbeDataSourceProxy(null, _mockCache.Object);
+
+        // Act
+        var result = await proxy.GetOpenDataFilesAsync(_cancellationToken);
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal("KboOpenData_0001_2025_01_01_Full.zip", result.First().Filename);
+    }
+
+    #endregion
+
+    #region ReadAsync Tests
+
+    [Fact]
+    public async Task ReadAsync_WithCacheAvailable_ShouldPreferCache()
+    {
+        // Arrange
+        var file = new CbeOpenDataFile("KboOpenData_0001_2025_01_01_Full.zip");
+        var cacheStream = new MemoryStream();
+        var sourceStream = new MemoryStream();
+
+        _mockCache.Setup(c => c.ReadAsync(file, _cancellationToken))
+            .ReturnsAsync(cacheStream);
+        _mockSource.Setup(s => s.ReadAsync(file, _cancellationToken))
+            .ReturnsAsync(sourceStream);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, _mockCache.Object);
+
+        // Act
+        var result = await proxy.ReadAsync(file, _cancellationToken);
+
+        // Assert
+        Assert.Same(cacheStream, result);
+        _mockCache.Verify(c => c.ReadAsync(file, _cancellationToken), Times.Once);
+        _mockSource.Verify(s => s.ReadAsync(file, _cancellationToken), Times.Never);
+    }
+
+    [Fact]
+    public async Task ReadAsync_WithCacheFailure_ShouldFallbackToSource()
+    {
+        // Arrange
+        var file = new CbeOpenDataFile("KboOpenData_0001_2025_01_01_Full.zip");
+        var sourceStream = new MemoryStream();
+
+        _mockCache.Setup(c => c.ReadAsync(file, _cancellationToken))
+            .ThrowsAsync(new FileNotFoundException("Cache file not found"));
+        _mockSource.Setup(s => s.ReadAsync(file, _cancellationToken))
+            .ReturnsAsync(sourceStream);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, _mockCache.Object);
+
+        // Act
+        var result = await proxy.ReadAsync(file, _cancellationToken);
+
+        // Assert
+        Assert.Same(sourceStream, result);
+        _mockCache.Verify(c => c.ReadAsync(file, _cancellationToken), Times.Once);
+        _mockSource.Verify(s => s.ReadAsync(file, _cancellationToken), Times.Once);
+    }
+
+    [Fact]
+    public async Task ReadAsync_WithOnlySource_ShouldUseSource()
+    {
+        // Arrange
+        var file = new CbeOpenDataFile("KboOpenData_0001_2025_01_01_Full.zip");
+        var sourceStream = new MemoryStream();
+
+        _mockSource.Setup(s => s.ReadAsync(file, _cancellationToken))
+            .ReturnsAsync(sourceStream);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, null);
+
+        // Act
+        var result = await proxy.ReadAsync(file, _cancellationToken);
+
+        // Assert
+        Assert.Same(sourceStream, result);
+        _mockSource.Verify(s => s.ReadAsync(file, _cancellationToken), Times.Once);
+    }
+
+    [Fact]
+    public async Task ReadAsync_WithOnlyCache_ShouldUseCache()
+    {
+        // Arrange
+        var file = new CbeOpenDataFile("KboOpenData_0001_2025_01_01_Full.zip");
+        var cacheStream = new MemoryStream();
+
+        _mockCache.Setup(c => c.ReadAsync(file, _cancellationToken))
+            .ReturnsAsync(cacheStream);
+
+        var proxy = new CbeDataSourceProxy(null, _mockCache.Object);
+
+        // Act
+        var result = await proxy.ReadAsync(file, _cancellationToken);
+
+        // Assert
+        Assert.Same(cacheStream, result);
+        _mockCache.Verify(c => c.ReadAsync(file, _cancellationToken), Times.Once);
+    }
+
+    [Fact]
+    public async Task ReadAsync_WithBothSourcesFailure_ShouldThrowAggregateException()
+    {
+        // Arrange
+        var file = new CbeOpenDataFile("KboOpenData_0001_2025_01_01_Full.zip");
+        var cacheException = new FileNotFoundException("Cache file not found");
+        var sourceException = new InvalidOperationException("Source unavailable");
+
+        _mockCache.Setup(c => c.ReadAsync(file, _cancellationToken))
+            .ThrowsAsync(cacheException);
+        _mockSource.Setup(s => s.ReadAsync(file, _cancellationToken))
+            .ThrowsAsync(sourceException);
+
+        var proxy = new CbeDataSourceProxy(_mockSource.Object, _mockCache.Object);
+
+        // Act & Assert
+        var aggregateException = await Assert.ThrowsAsync<AggregateException>(() =>
+            proxy.ReadAsync(file, _cancellationToken));
+
+        Assert.Equal(2, aggregateException.InnerExceptions.Count);
+        Assert.Contains(cacheException, aggregateException.InnerExceptions);
+        Assert.Contains(sourceException, aggregateException.InnerExceptions);
+        Assert.Equal("Failed to read the OpenDataFile. See inner exceptions for more details.", 
+            aggregateException.Message);
+    }
+
+    #endregion
+}
